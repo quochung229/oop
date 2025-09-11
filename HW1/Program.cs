10d85b5f8d1c099221bb32d1f7d878ca8224c39c
Đặng Quốc Hưng <quochung2292006@gmail.com>
2025-09-11 22:00:26 +0700

    Upload homework HW1 -> HW5

---
+++ HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string hoten = "Đặng Quốc Hưng";
+        string maSV = "10124155";
+        string lop = "12424TN";
+        string githubser = "quochung229";
+        string email = "quochung2292006@gmail.com";
+
+        Console.WriteLine("Họ tên:\t" + hoten);
+        Console.WriteLine("Mã SV:\t" + maSV);
+        Console.WriteLine("Lớp:\t" + lop);
+        Console.WriteLine("GitHub:\t" + githubuser);
+        Console.WriteLine("Email:\t" + email);
+
+        Console.ReadKey();
+    }
